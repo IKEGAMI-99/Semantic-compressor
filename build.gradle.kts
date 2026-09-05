efa3a51a392e2bc6b50d24b3b4af07acdcfd8bca
IKEGAMI-99 <hato.jp@gmail.com>
2026-09-05 19:14:57 +0900

    Pin Android build to stable API 36 toolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 buildscript {
     dependencies {
-        // AGP 9 uses built-in Kotlin. Pin its KGP runtime to the same version as Compose Compiler.
+        // AGP 9 uses built-in Kotlin. Keep Compose Compiler/KGP aligned.
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.21")
     }
 }
 
 plugins {
-    id("com.android.application") version "9.4.0" apply false
+    id("com.android.application") version "9.3.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
 }
